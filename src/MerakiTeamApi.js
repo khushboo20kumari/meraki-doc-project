6f631381dc27f750f345cd16228fd7109fa6be3e
khushboo kumari <khushboo20@navgurukul.org>
2023-10-07 10:59:59 +0530

    use map and display data useing grid in meterial ui

--- src/MerakiTeamApi.js
+++ src/MerakiTeamApi.js
@@ -1,5 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 import { useEffect, useState } from "react";
+import { Grid, Typography } from "@mui/material"
+// import { experimentalStyled as styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
 function MerakiTeam() {
 
@@ -8,27 +11,35 @@ function MerakiTeam() {
   const fetchData = () => {
     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357&')
 
-     .then(response => response.json())
-     
-     .then(movieData => {
-         setData(movieData.results.slice(0, 10))
-         console.log(movieData,"data");
+      .then(response => response.json())
+
+      .then(movieData => {
+        setData(movieData.results)
+        console.log(movieData, "data");
       })
-     
+
       .catch(error => {
-         console.error('Error fetching data:', error)
-       });
-   };
+        console.error('Error fetching data:', error)
+      });
+  };
 
-   useEffect(() => {
-      fetchData();
-   }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
 
-   console.log(data,"data")
   return (
     <>
-
+      <Grid container spacing={2} align="center">
+        {data.map(item => (
+          <Grid item xs={3} sm={6} md={3} lg={3} key={item.id}>
+            <Paper>
+              <img style={{maxWidth:"400px",maxHeight:"300px"}} src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} className="img" />
+              <Typography variant="h6">{item.title}</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
     </>
   )
 }
