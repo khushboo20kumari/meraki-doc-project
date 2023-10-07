259f05bdf09867f169b90a658157bbd1c0243fad
khushboo kumari <khushboo20@navgurukul.org>
2023-10-07 15:15:40 +0530

    use post method in APi posting data in API

--- src/PostMethod.js
+++ src/PostMethod.js
@@ -1,55 +1,20 @@
-// import { type } from "@testing-library/user-event/dist/type";
+import { type } from "@testing-library/user-event/dist/type";
 
-// function PostMethod(){
-//     fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357&",{
-//        method:"POST",
+function PostMethod(){
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357&",{
+       method:"POST",
 
-//        headers:{
-//         Accept:"application\json",
-//         Content-type:"applicaton\json",
-//        }
+       headers:{
+        Accept:"application\json",
+        Content:"applicaton\json",
+       }
 
-//     })
-//     return(
-//         <>
-
-//         </>
-//     )
-// }
-// export default PostMethod;
-
-
-import React from "react";
-
-import { useState, useEffect } from "react";
-
-const OurPartner = () => {
-    const [partner, setPartner] = useState([]);
-
-    useEffect(() => {
-        axios
-            .get(
-                "https://navgurukul.github.io/tarabai-shinde/data/meraki_partners.json"
-            )
-            .then((response) => {
-                setPartner(response.data);
-            });
-    }, []);
-
-    return (
+    })
+    return(
         <>
 
-            {Object.keys(partner).map((item) => {
-                return(
-                   <div>{item.name}</div>
-                )
-            })}  
-
-    </>
-    );
-};
-
-export default OurPartner;
-
-
+        </>
+    )
+}
+export default PostMethod;
 
