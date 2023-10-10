c977550b72014b99b999bcd1d1a50d3a52bafc07
khushboo kumari <khushboo20@navgurukul.org>
2023-10-10 10:43:24 +0530

    use sort method

--- src/PhoneBook.js
+++ src/PhoneBook.js
@@ -1,58 +1,105 @@
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Box, Card, Typography } from "@mui/material";
 import { useState } from "react";
 
 function PhoneBook() {
 
-    const [name,setName]=useState('')
+    const [name, setName] = useState('')
 
-    const [lastName,setLastName]=useState('')
+    const [lastName, setLastName] = useState('')
 
-    const [phone,setPhone]=useState('')
+    const [phone, setPhone] = useState('')
 
-    const [data,setData]=useState([])
-    
-    
-    const HandleName=(e)=>{
+    const [data, setData] = useState([])
+
+
+    const HandleName = (e) => {
 
         setName(e.target.value)
     }
 
-    const  HandleLastName=(e)=>{
+    const HandleLastName = (e) => {
         setLastName(e.target.value)
     }
 
-    const HandlePhone=(e)=>{
+    const HandlePhone = (e) => {
         setPhone(e.target.value)
     }
-    let allData={name,lastName,phone}
 
-    const HandleAdd=(e)=>{
-    
+
+    const HandleAdd = (e) => {
+
         setName('')
         setLastName('')
         setPhone('')
-        setData([...data,allData])
+        setData([...data, { name, lastName, phone }])
 
-        
+
+
+
     }
 
-    console.log(data,"data")
+    // console.log(data, "data")
+    let data1=data.sort((a, b) => (b.name > a.name ? 1 : -1))
 
 
     return (
 
         <>
-            
 
-                <TextField onChange={HandleName}></TextField><br></br>
 
-                <TextField onChange={HandleLastName}></TextField><br></br>
 
-                <TextField onChange={HandlePhone}></TextField><br></br>
+            <Box style={{ marginTop: "150px" }}>
 
-                <Button onClick={HandleAdd} variant="contained">add</Button><br></br>
 
-            
+                <Card style={{ width: "30%", margin: "auto", textAlign: "center" }}>
+
+                    <Typography variant="h4">Phone Book App</Typography>
+
+                    <TextField onChange={HandleName} value={name}></TextField><br></br>
+
+                    <TextField onChange={HandleLastName} value={lastName}></TextField><br></br>
+
+                    <TextField onChange={HandlePhone} value={phone}></TextField><br></br>
+
+                    <Button onClick={HandleAdd} variant="contained">add</Button><br></br>
+
+
+
+                </Card>
+
+                <Box style={{ width: "30%", margin: "auto", textAlign: "center",marginTop:"80px"}}>
+                    <table border="1" style={{width:"100%"}}>
+                        <thead>
+
+                            <tr>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Phone</th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+
+                            {data1.map((item, index) => (
+
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.phone}</td>
+                                </tr>
+                            ))}
+
+                        </tbody>
+
+                    </table>
+                
+                    </Box>
+            </Box>
+
+
+
+
+
         </>
 
     )
